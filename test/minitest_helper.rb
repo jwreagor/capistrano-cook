9d4e05155c8ea4a797febb060352962ec111e839
Justin Reagor <justinwr@gmail.com>
2014-08-01 03:22:35 -0400

    Fix borked test

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,4 +2,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest/autorun'
 require 'chef_zero/server'
 require 'capistrano/chef'
-require 'capistrano/chef/test_case'
+require 'capistrano/chef/test_helpers'
